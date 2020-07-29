2141f791f5bfaf21fe3b6ce16ea0ecf05c857a29
root <root@c18f1211940a>
2020-07-29 22:02:43 +0000

    FourSquare

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		Robot rob = new Robot();
+		rob.setSpeed(250);
+		rob.penDown();
+		while(true) {
+		rob.move(200);
+		rob.turn(95);
+		rob.setRandomPenColor();
 	
 		
-		
+		}
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,22 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot rob = new Robot();
 		// 5. Set your robot's pen down 
-		
+		rob.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
-		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		rob.setSpeed(100);
+		for (int i = 1; i <= 75; i++ ) {		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+			
+			
 			// 7. Change the pen color to random
-		
+			rob.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			rob.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		rob.turn(360/7);
 			// 8. Set the pen width to i
-			
+		rob.setPenWidth(1);
+	}
 	}
 }
